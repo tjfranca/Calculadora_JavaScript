36232d5f6f1da9ba5e166115b8d88c6d4099ae84
Tiago França <99991364+tjfranca@users.noreply.github.com>
2022-03-16 15:14:29 -0300

    Add files via upload
    Upload da aula 6 de JavaScript.

---
+++ aula 6 - aprofundamento em funcoes/script.js
@@ -0,0 +1,27 @@
+/*tipos de função
+//declarativas
+
+function funcao() {
+    console.log('tudo certo, jovem!')
+}
+
+funcao(); */
+
+/*expressões de funções
+//com nomeação
+
+var funcao = function() {
+    console.log('tudo certo,jovem!')
+} 
+
+funcao(); */
+
+/*arrow function
+
+var funcao = () => {
+    console.log('sou uma arrow function')
+}
+funcao(); */
+
+//funções aritméticas
+
